e440447344f3922ec663f76c9b5ccebfd9ab4ac9
Honza Kubita <honza.kubita@gmail.com>
2023-08-23 14:52:00 +0200

    miner v1 test

---
+++ miner.lua
@@ -0,0 +1,63 @@
+-- miner.lua
+
+print("Enter d1 (length):")
+local d1 = read()
+
+print("Enter d2 (width):")
+local d2 = read()
+
+print("Enter d3 (depth):")
+local d3 = read()
+
+local status = "Mining"
+
+function printStatus()
+  term.clear()
+  print("------ Miner Status ------")
+  print("Fuel: "..turtle.getFuelLevel())
+  print("Status: "..status)
+end
+
+function getRefuel()
+
+end
+
+function tryToMove()
+  turtle.forward()
+end
+
+local flip = 0
+
+for i = 1, d3, 1 do -- For each layer
+
+  for j = 0, d2, 1 do -- For width
+
+    for k = 1, d1, 1 do
+      turtle.digDown()
+      tryToMove()
+      printStatus()
+    end
+  
+    if i + flip % 2 == 0 then -- Even row
+      turtle.turnRight()
+      tryToMove()
+      turtle.turnRight()
+    else
+      turtle.turnLeft()
+      tryToMove()
+      turtle.turnLeft()
+    end
+
+  end
+
+  -- Rotate at the end
+  turtle.turnLeft()
+  turtle.turnLeft()
+
+  if flip == 0 then
+    flip = 1
+  else
+    flip = 0
+  end
+
+end
